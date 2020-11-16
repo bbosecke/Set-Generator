41bd6d5c8a63883e4b2e3737d7ecc6f5011ef783
Brodie Bosecke <brodie.bosecke.yt@gmail.com>
2020-11-17 12:11:11 +1300

    All methods working, new method printContents (prints the contents of ArrayList and HashMap

--- SetGenerator.java
+++ SetGenerator.java
@@ -1,44 +1,82 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
+import java.util.Collections;
 
 class SetGenerator{
 
     ArrayList<Integer> arr;
+    HashMap<Integer, Integer> hash;
+    int i = 0;
 
     /** Initialize your data structure here. */
     public SetGenerator() {
         arr = new ArrayList<Integer>();
+        hash = new HashMap<Integer, Integer>();
     }
 
-    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. 
+     * 
+     * Checks the HashMap for val trying to be inserted.
+     * If it does not exist in the HashMap, then add to both (end of) ArrayList and HashMap
+    */
     public boolean insert(int val) {
-        if(arr.contains(val) == true){
+        if(hash.get(val) != null){
             return false;
         }
+        //int i = arr.size();
         arr.add(val);
+        hash.put(val, i);
+        i++;
+
         return true;
     }
 
     /** Removes a value from the set. Returns true if the set contained the specified element. */
     public boolean remove(int val) {
-        if(arr.contains(val) == true){
-            arr.remove(val);
-            return true;
+        Integer index = hash.get(val);
+        //If index doesn't exist, return false
+        if(index == null){
+            return false;
         }
-        return false;
+        //System.out.println(hash.entrySet());
+
+        hash.remove(val);
+
+        //beacuse the value being removed isn't in the correct pos in the ArrayList, need to swap
+        //to make it the last pos in list
+        int arrSize = arr.size();
+        //System.out.println(arrSize);
+        //System.out.println(index);
+        int posLast = arrSize-1;
+        //System.out.println(posLast);
+        Collections.swap(arr, index, posLast);
+        arr.remove(posLast);
+        //System.out.println(arr.size());
+        //System.out.println(hash.entrySet());
+
+        return true;
     }
 
     /** Get a random element from the set. */
     public int getRandom() {
         Random rand = new Random();
         int randPos = rand.nextInt(arr.size());
-        return arr.get(randPos);
+        int key = arr.get(randPos);
+        return key;
     }   
 
-    public void contents(){
+    /**
+    * contents prints the current contents of the ArrayList and HashMap for testing purposes,
+    * and ensuring that everything is in there that should be contained
+    */
+    public void printContents(){
+        System.out.print("ArrayList contents: ");
         for(int i = 0; i < arr.size(); i++){
-            System.out.println("Printing contents: " + arr.get(i));
+            System.out.print(arr.get(i) + " " );
         }
+        System.out.println();
+        System.out.println(hash.entrySet());
     }
 }
 
